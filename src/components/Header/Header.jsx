2c8a41066c5553563bcd9fab513c128aeb15fff6
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-08-23 07:50:42 +0300

    Добавляет хедер для главной страницы

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,40 @@
 import './Header.css';
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
+import { Link } from 'react-router-dom';
+// import Navigation from '../Navigation/Navigation';
+import logo from '../../images/header__logo.svg';
+// import account_icon from '../../images/account__icon.svg';
 
 const Header = () => {
   return (
-    <div>
-      <Navigation />
-    </div>
+    <header className="header header_colored">
+      <div className="header__content">
+        <Link
+          className="header__logo"
+          to="/"
+        >
+          <img
+            src={logo}
+            alt="Логотип"
+          />
+        </Link>
+        {/* <Navigation /> */}
+        <nav className="header__auth">
+          <Link
+            className="header__link"
+            to="/sign-up"
+          >
+            Регистрация
+          </Link>
+          <Link
+            className="header__link header__link_sign-in"
+            to="/sign-in"
+          >
+            Войти
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
