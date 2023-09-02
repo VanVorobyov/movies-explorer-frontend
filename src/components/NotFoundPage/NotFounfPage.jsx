2a22b3d21f441d457d51e457d725ca291d7ea26b
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-09-02 21:58:58 +0300

    Добавляет адаптив для мобильных устройств с разрешение 320 + фикс вертски

--- src/components/NotFoundPage/NotFounfPage.jsx
+++ src/components/NotFoundPage/NotFounfPage.jsx
@@ -8,13 +8,13 @@ const NotFoundPage = () => {
       <div className="error-page__text-wrap">
         <h1 className="error-page__title">404</h1>
         <p className="error-page__subtitle">Страница не найдена</p>
-        <Link
-          to="/"
-          className="error-page__link"
-        >
-          Назад
-        </Link>
       </div>
+      <Link
+        to="/"
+        className="error-page__link"
+      >
+        Назад
+      </Link>
     </section>
   );
 };
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Promo.css";
-import promo_logo from "../../images/promo__logo.svg";
+import React from 'react';
+import './Promo.css';
+import promo_logo from '../../images/promo__logo.svg';
 
 const Promo = () => {
   return (
@@ -9,11 +9,18 @@ const Promo = () => {
         <div className="promo__text-wrap">
           <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
           <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-          <a href="#about-project" className="promo__button">
+          <a
+            href="#about-project"
+            className="promo__button"
+          >
             Узнать больше
           </a>
         </div>
-        <img className="promo__img" alt="Логотип промо" src={promo_logo} />
+        <img
+          className="promo__img"
+          alt="Логотип промо"
+          src={promo_logo}
+        />
       </div>
     </section>
   );
