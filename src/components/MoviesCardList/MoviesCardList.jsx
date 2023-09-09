ca83db8dbdaa51b6e0b5369ed875fa7369dd0bff
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-09-09 09:45:54 +0300

    Добавляет изменение количества показываемых фильмов при изменении размера окна + функциональность кнопки "Еще"

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 const MoviesCardList = ({ movies }) => {
+  const [isNumberOfMovies, setNumberOfMovies] = useState(0);
+
+  function handleNumberOfFilve() {
+    const display = window.innerWidth;
+    if (display > 1023) {
+      setNumberOfMovies(12);
+    } else if (display > 698) {
+      setNumberOfMovies(8);
+    } else {
+      setNumberOfMovies(5);
+    }
+  }
+
+  function handleShowMore() {
+    const display = window.innerWidth;
+    if (display > 1023) {
+      setNumberOfMovies(isNumberOfMovies + 3);
+    } else if (display > 698) {
+      setNumberOfMovies(isNumberOfMovies + 2);
+    } else {
+      setNumberOfMovies(isNumberOfMovies + 1);
+    }
+  }
+
+  useEffect(() => {
+    handleNumberOfFilve();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.addEventListener('resize', handleNumberOfFilve);
+    }, 500);
+  });
+
   return (
     <section className="movies-card-list">
       <div className="movies-card-list__content">
         <ul className="movies-cards">
-          {movies.slice(0, 12).map((movie) => {
+          {movies.slice(0, isNumberOfMovies).map((movie) => {
             return (
               <MoviesCard
                 key={movie.id}
@@ -18,7 +52,8 @@ const MoviesCardList = ({ movies }) => {
         </ul>
         <button
           type="button"
-          className="movies-card-list__button "
+          className={`movies-card-list__button ${movies.length <= isNumberOfMovies && 'movies-card-list__button_hidden'}`}
+          onClick={handleShowMore}
         >
           Ещё
         </button>
