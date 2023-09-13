65a1e82b9378d04466c7458580baaeb8008659d5
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-09-13 22:58:42 +0300

    Добавляет фильтрацию короткометражных сохраненных фильмов

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
+import FilterMovies from '../../utils/FilterMovies';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 const SavedMovies = (props) => {
-  const { movies, savedMovies, onMovieDelete, onSaveMovie, onBurgerButtonClick } = props;
+  const { movies, savedMovies, onMovieDelete, onBurgerButtonClick } = props;
+
+  const [isShortMovies, setIsShortMovies] = useLocalStorage('isShortMovies', false);
+  const [shortSavedMovies, setShortSavedMovies] = useState([]);
+
+  const handleCheckboxClick = () => {
+    setIsShortMovies((prev) => !prev);
+  };
+
+  const handleShortMovies = () => {
+    localStorage.setItem('filteredSavedMovies', JSON.stringify(FilterMovies(savedMovies)));
+  };
+
+  useEffect(() => {
+    if (isShortMovies) {
+      setShortSavedMovies(JSON.parse(localStorage.getItem('filteredSavedMovies')));
+    } else {
+      setShortSavedMovies(savedMovies);
+    }
+  }, [isShortMovies, savedMovies]);
+
   return (
     <>
       <Header onClick={onBurgerButtonClick} />
       <main>
-        <SearchForm />
+        <SearchForm
+          handleCheckboxClick={handleCheckboxClick}
+          handleShortMovies={handleShortMovies}
+          isShortMovies={isShortMovies}
+        />
         <MoviesCardList
           movies={movies}
-          savedMovies={savedMovies}
+          savedMovies={shortSavedMovies}
           onMovieDelete={onMovieDelete}
         />
       </main>
